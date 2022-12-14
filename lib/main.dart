33159af65a4e21a503be468108a2bc7f1fdf07ff
ricarthlima <rrsl@cin.ufpe.br>
2022-12-14 15:05:21 -0300

    v1.3 - Instalando o Cloud Firestore

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,21 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'firebase_options.dart';
 
-void main() {
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
+
   runApp(const MyApp());
+
+  FirebaseFirestore firestore = FirebaseFirestore.instance;
+  firestore.collection("Só para testar").doc("Estou testando").set({
+    "funcionou?": true,
+  });
 }
 
 class MyApp extends StatelessWidget {
