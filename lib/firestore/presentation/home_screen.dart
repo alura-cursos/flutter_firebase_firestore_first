98e03737caa3228c6ba3ed752a85ec5495f45e4f
ricarthlima <rrsl@cin.ufpe.br>
2022-12-14 15:10:01 -0300

    v2.2 - Importando Tela Home

---
+++ lib/firestore/presentation/home_screen.dart
@@ -0,0 +1,111 @@
+import 'package:flutter/material.dart';
+import '../models/listin.dart';
+
+class HomeScreen extends StatefulWidget {
+  const HomeScreen({super.key});
+
+  @override
+  State<HomeScreen> createState() => _HomeScreenState();
+}
+
+class _HomeScreenState extends State<HomeScreen> {
+  final List<Listin> listListins = [];
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text("Listin - Feira Colaborativa"),
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: () {
+          showFormModal();
+        },
+        child: const Icon(Icons.add),
+      ),
+      body: (listListins.isEmpty)
+          ? const Center(
+              child: Text(
+                "Nenhuma lista ainda.\nVamos criar a primeira?",
+                textAlign: TextAlign.center,
+                style: TextStyle(fontSize: 18),
+              ),
+            )
+          : ListView(
+              children: List.generate(
+                listListins.length,
+                (index) {
+                  Listin model = listListins[index];
+                  return ListTile(
+                    leading: const Icon(Icons.list_alt_rounded),
+                    title: Text(model.name),
+                    subtitle: Text(model.id),
+                  );
+                },
+              ),
+            ),
+    );
+  }
+
+  showFormModal() {
+    // Labels à serem mostradas no Modal
+    String title = "Adicionar Listin";
+    String confirmationButton = "Salvar";
+    String skipButton = "Cancelar";
+
+    // Controlador do campo que receberá o nome do Listin
+    TextEditingController nameController = TextEditingController();
+
+    // Função do Flutter que mostra o modal na tela
+    showModalBottomSheet(
+      context: context,
+
+      // Define que as bordas verticais serão arredondadas
+      shape: const RoundedRectangleBorder(
+        borderRadius: BorderRadius.vertical(
+          top: Radius.circular(24),
+        ),
+      ),
+      builder: (context) {
+        return Container(
+          height: MediaQuery.of(context).size.height,
+          padding: const EdgeInsets.all(32.0),
+
+          // Formulário com Título, Campo e Botões
+          child: ListView(
+            children: [
+              Text(title, style: Theme.of(context).textTheme.headline5),
+              TextFormField(
+                controller: nameController,
+                decoration:
+                    const InputDecoration(label: Text("Nome do Listin")),
+              ),
+              const SizedBox(
+                height: 16,
+              ),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.end,
+                children: [
+                  TextButton(
+                    onPressed: () {
+                      Navigator.pop(context);
+                    },
+                    child: Text(skipButton),
+                  ),
+                  const SizedBox(
+                    width: 16,
+                  ),
+                  ElevatedButton(
+                      onPressed: () {
+                        //TODO: Implementar adição
+                      },
+                      child: Text(confirmationButton)),
+                ],
+              )
+            ],
+          ),
+        );
+      },
+    );
+  }
+}
